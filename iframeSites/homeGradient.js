cf1ac421c186642625a9c12d0b00fdfe5ffa7360
EmilSkjetne <emil.skjetne@gmail.com>
2021-09-06 12:29:06 +0200

    Har laget layout

---
+++ iframeSites/homeGradient.js
@@ -0,0 +1,15 @@
+
+//------------------Home
+
+
+function subMenu(){
+
+    var c = document.getElementById("kontaktOssMenu");
+
+    if(c.style.display === "none") {
+        c.style.display = "block";
+    }
+    else{
+        c.style.display = "none";
+    }
+} 
